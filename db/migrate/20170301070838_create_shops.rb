1252820fa56e192d0e7d7bf8f36da046c5bebf76
vietnh3005 <vietnh3005@gmail.com>
2017-03-01 23:33:08 +0700

    Add static page

--- db/migrate/20170301070838_create_shops.rb
+++ db/migrate/20170301070838_create_shops.rb
@@ -2,7 +2,7 @@ class CreateShops < ActiveRecord::Migration[5.0]
   def change
     create_table :shops do |t|
       t.string :name
-      t.srting :address
+      t.string :address
       t.references :location, foreign_key: true
 
       t.timestamps
