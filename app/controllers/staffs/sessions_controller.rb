c262bab21e225e489369fa6f03a5e67454a36a33
vietnh3005 <vietnh3005@gmail.com>
2017-03-13 09:53:57 +0700

    Improve login view

--- app/controllers/staffs/sessions_controller.rb
+++ app/controllers/staffs/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Staffs::SessionsController < Devise::SessionsController
-  layout "staffs_layout/staffs_layout"
+  layout "login_layout/login_layout"
   protected
   def after_sign_in_path_for resource
     staff_root_path
