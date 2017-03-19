a28cd2e58cd2011efda4d9f80b794bc802d1eae1
vietnh3005 <vietnh3005@gmail.com>
2017-03-20 03:32:37 +0700

    Styling flash message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,7 @@
 //= require bootstrap-sprockets
 //= require bxslider
 //= require bootstrap-datepicker
+//= require toastr
 
 jQuery(document).ready(function($){
 
@@ -44,4 +45,22 @@ jQuery(document).ready(function($){
         });
       }
   });
+});
+
+
+$(document).ready(function() {
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "positionClass": "toast-bottom-right",
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
 });
